cef5c805967f2a99dd6afb8b68a0fe6a1dcd75b8
Oscar Sauceda <osaucedab.etp@gmail.com>
2022-07-20 15:56:02 -0700

    debugging mongodb env

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const port = process.env.PORT || 3000;
+  console.log('mongo db env', process.env.MONGODB_URI);
   await app.listen(port);
 }
 bootstrap();
